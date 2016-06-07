df547eea018f6e4d4edbed2b73cb073090766e1b
志庄 杨 <yzz0427@outlook.com>
2016-06-07 20:45:15 +0800

    Add a story of Cao Mai into the console. Add a link to the github page. PS:  Cao Mai is coming in the next version...

--- js/styles.js
+++ js/styles.js
@@ -2,12 +2,13 @@ $(document).ready(function () {
 	var html = $('html');
 	var blessings=$('div.blessings');
 	var header = $('header');
+	var footer=$('footer');
 	var styles = function () {
 		var height = 0;
 		if(parseInt(html.css('width')) >= 641) {
-			height = parseInt(html.css('height')) - parseInt(header.css('height'))  - 60;
+			height = html.height() - header.height()-footer.height()  - 60;
 		} else {
-			height = parseInt(html.css('height')) - parseInt(header.css('height'))  - 40;
+			height = html.height() - header.height()-footer.height()   - 40;
 		}
 		blessings.css('height', height + 'px');
 	};
