661fe770bb70e40c8e6fd7bf62ad01c9ff2d7559
Jerry <842791049@qq.com>
2016-06-07 22:21:53 +0800

    optimize

---
+++ js/console.js
@@ -0,0 +1,3 @@
+console.log('Cao Mai is no longer in the world...');
+console.log('But...');
+console.log('She will be back...');
--- js/styles.js
+++ js/styles.js
@@ -1,16 +1,16 @@
 $(document).ready(function () {
 	var html = $('html');
-	var blessings=$('div.blessings');
+	var blessings = $('div.blessings');
 	var header = $('header');
-	var footer=$('footer');
+	var footer = $('footer');
 	var styles = function () {
 		var height = 0;
-		if(parseInt(html.css('width')) >= 641) {
+		if(html.width() >= 641) {
 			height = html.height() - header.height()-footer.height()  - 60;
 		} else {
 			height = html.height() - header.height()-footer.height()   - 40;
 		}
-		blessings.css('height', height + 'px');
+		blessings.height(height);
 	};
 	styles();
 	$(window).load(styles).resize(styles);
