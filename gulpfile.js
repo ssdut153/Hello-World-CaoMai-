1a9d3eaca34e687810a22be4fbe7704def2c5fd0
Jerry <842791049@qq.com>
2016-06-09 08:23:16 +0800

    use pug and less

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,10 @@
-var fs = require('fs');
+var cssclean = require('gulp-clean-css');
+var fs = require('graceful-fs');
 var glob = require('glob');
 var gulp = require('gulp');
+var less = require('gulp-less');
 var path = require('path');
+var pug = require('gulp-pug');
 var plugins = require('gulp-load-plugins')();
 var sequence = require('run-sequence');
 
@@ -10,99 +13,115 @@ var pkg = require('./package.json');
 var dirs = pkg['h5bp-configs'].directories;
 
 gulp.task('archive:create_archive_dir',function () {
-	fs.mkdirSync(path.resolve(dirs.archive), '0755');
+    fs.mkdirSync(path.resolve(dirs.archive), '0755');
 });
 
 gulp.task('archive:zip', function (done) {
 
-	var archiveName = path.resolve(dirs.archive, pkg.name + '_v' + pkg.version + '.zip');
-	var archiver = require('archiver')('zip');
-	var files = glob.sync('**/*.*', {
-		'cwd': dirs.dist,
-		'dot': true // include hidden files
-	});
-	var output = fs.createWriteStream(archiveName);
+    var archiveName = path.resolve(dirs.archive, pkg.name + '_v' + pkg.version + '.zip');
+    var archiver = require('archiver')('zip');
+    var files = glob.sync('**/*.*', {
+        'cwd': dirs.dist,
+        'dot': true // include hidden files
+    });
+    var output = fs.createWriteStream(archiveName);
 
-	archiver.on('error', function (error) {
-		done();
-		throw error;
-	});
+    archiver.on('error', function (error) {
+        done();
+        throw error;
+    });
 
-	output.on('close', done);
+    output.on('close', done);
 
-	files.forEach(function (file) {
+    files.forEach(function (file) {
 
-		var filePath = path.resolve(dirs.dist, file);
+        var filePath = path.resolve(dirs.dist, file);
 
-		// `archiver.bulk` does not maintain the file
-		// permissions, so we need to add files individually
-		archiver.append(fs.createReadStream(filePath), {
-			'name': file,
-			'mode': fs.statSync(filePath).mode
-		});
+        // `archiver.bulk` does not maintain the file
+        // permissions, so we need to add files individually
+        archiver.append(fs.createReadStream(filePath), {
+            'name': file,
+            'mode': fs.statSync(filePath).mode
+        });
 
-	});
+    });
 
-	archiver.pipe(output);
-	archiver.finalize();
+    archiver.pipe(output);
+    archiver.finalize();
 
 });
 
 gulp.task('clean', function (done) {
-	require('del')([
-		dirs.archive,
-		dirs.dist
-	]).then(function () {
-		done();
-	});
+    require('del')([
+        dirs.archive,
+        dirs.dist
+    ]).then(function () {
+        done();
+    });
+});
+
+gulp.task('pug', function () {
+    return gulp
+        .src([dirs.src + '/*.pug'])
+        .pipe(pug({
+            /*pretty: true*/
+        }))
+        .pipe(gulp.dest(dirs.dist));
+});
+
+gulp.task('less',function () {
+    return gulp
+        .src([dirs.src + '/css/*.less'])
+        .pipe(less())
+        .pipe(cssclean())
+        .pipe(gulp.dest(dirs.dist + '/css'));
 });
 
 gulp.task('copy',[
-	'copy:index.html',
-	'copy:jquery',
-	'copy:license',
-	'copy:misc'
+    'copy:jquery',
+    'copy:license',
+    'copy:misc'
 ]);
 
 gulp.task('copy:license', function () {
-	return gulp.src('LICENSE.txt')
-		.pipe(gulp.dest(dirs.dist));
-});
-
-gulp.task('copy:index.html', function () {
-	return gulp.src(dirs.src + '/index.html')
-		.pipe(plugins.replace(/{{JQUERY_VERSION}}/g, pkg.devDependencies.jquery))
-		.pipe(gulp.dest(dirs.dist));
+    return gulp
+        .src('LICENSE.txt')
+        .pipe(gulp.dest(dirs.dist));
 });
 
 gulp.task('copy:jquery', function () {
-	return gulp.src(['node_modules/jquery/dist/jquery.min.js'])
-		.pipe(plugins.rename('jquery-' + pkg.devDependencies.jquery + '.min.js'))
-		.pipe(gulp.dest(dirs.dist + '/js/vendor'));
+    return gulp
+        .src(['node_modules/jquery/dist/jquery.min.js'])
+        .pipe(gulp.dest(dirs.dist + '/js/vendor'));
 });
 
 gulp.task('copy:misc', function () {
-	return gulp.src([
-		dirs.src + '/**/*',
-		'!' + dirs.src + '/index.html'
-	], {
-		dot: true
-	}).pipe(gulp.dest(dirs.dist));
+    return gulp
+        .src([
+            dirs.src + '/**/*',
+            '!' + dirs.src + '/*.pug',
+            '!' + dirs.src + '/css/*.less'
+        ], {
+            dot: true
+        })
+        .pipe(gulp.dest(dirs.dist));
 });
 
 gulp.task('archive', function (done) {
-	sequence(
-		'build',
-		'archive:create_archive_dir',
-		'archive:zip',
-		done);
+    sequence(
+        'build',
+        'archive:create_archive_dir',
+        'archive:zip',
+        done);
 });
 
 gulp.task('build', function (done) {
-	sequence(
-		['clean'],
-		'copy',
-		done);
+    sequence(
+        ['clean'],
+        'copy',
+        'pug',
+        'less',
+        done);
 });
 
 gulp.task('default', ['build']);
